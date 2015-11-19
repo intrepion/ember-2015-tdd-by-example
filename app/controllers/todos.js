1ac0c803b506c4718c1c649ceb16d79bec1b40cc
Oliver Forral <intrepion@gmail.com>
2015-11-19 00:18:06 -0800

    Made tests pass

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    assign: function(todo) {
+      todo.set('status_code', 2);
+    }
+  },
   unassigned: function() {
     return this.get('model').filter(function(model) {
       return 1 === model.status_code;
     });
-  }.property(),
+  }.property('model.@each.status_code'),
   assigned: function() {
     return this.get('model').filter(function(model) {
       return 2 === model.status_code;
     });
-  }.property()
+  }.property('model.@each.status_code')
 });
