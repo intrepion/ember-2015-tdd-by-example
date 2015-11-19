4b3838e291a66b61e355be833554f8958b485733
Oliver Forral <intrepion@gmail.com>
2015-11-19 00:04:41 -0800

    Made unit tests pass

--- app/models/todo.js
+++ app/models/todo.js
@@ -4,5 +4,5 @@ export default Ember.Object.extend({
   status: function() {
     var status_code = this.get('status_code');
     return 1 === status_code ? 'Unassigned' : 'Assigned';
-  }.property()
+  }.property('status_code')
 });
