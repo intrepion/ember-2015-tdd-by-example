c7ac5c32766317ab6c1f50f9c5b0f388d939de47
Oliver Forral <intrepion@gmail.com>
2015-11-19 01:19:15 -0800

    Checking for clicking on details

--- tests/acceptance/assignment-test.js
+++ tests/acceptance/assignment-test.js
@@ -47,3 +47,11 @@ test('clicking the assign button will move item from unassigned to assigned', fu
     assert.equal(assigned.length, 2);
   });
 });
+
+test('clicking toggle link will show details for given item', function(assert) {
+  visit('/');
+  click('.unassigned .cards:eq(0) .toggle_link');
+  andThen(function() {
+    assert.equal(currentURL(), '/todo/1');
+  });
+});
